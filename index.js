8b5b04da9ba0085729a3a743b5dc532f898ea850
Gori <49369600+hafidzamr@users.noreply.github.com>
2021-07-30 23:09:15 +0000

    remove package-lock.json

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
 const RunningServer = require("./server");
+
+const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log("Bot Started");
 });
 
 client.on("voiceStateUpdate", (oldState, newState) => {
+  console.log(newState)
   if (newState.channelID === newState.guild.afkChannelID) {
     setTimeout(() => {
       newState.member.voice.kick();
-    }, 5000);
+    }, 3000);
   }
 });
 
 RunningServer();
-
 const token = process.env['TOKEN']
-
 client.login(token);
