946ce838f6b12d679533567271a56d1c2b2292fa
Gori <49369600+hafidzamr@users.noreply.github.com>
2021-07-31 21:56:14 +0000

    adding more list

--- store.js
+++ store.js
@@ -6,7 +6,12 @@ module.exports = {
       `Cuma tiga cinta yang tdk akan pernah berakhir: cinta ibu pd anaknya,cinta fitri dan cintaku padamu. ${mention}`,
       `Sejak kenal kamu, bawaanya pengen belajar terus deh. Belajar jadi yang terbaik buat kamu. ${mention}`,
       `Dulu belajar untuk menuntut ilmu setinggi2nya, kalo sekarang aku belajar untuk menuntut cintamu setinggi2nya. ${mention}`,
-      `Aku yakin kita sebenernya ga saling suka, tapi saling sayang. ${mention}`
+      `Aku yakin kita sebenernya ga saling suka, tapi saling sayang. ${mention}`,
+      `Sekeras-kerasnya Ibu Kota Jakarta, masih lebih keras tekadku untuk mendapatkan cintamu~. ${mention}`,
+      `senyuman kamu tuh ibarat SUSU BENDERA nikmat nya hingga tetes terakhir. ${mention}`,
+      `kenal kamu kaya kenal jelangkung. sekali datang ga pernah bisa pergi dari hidupku. ${mention}`,
+      `nternet bikin hidup lebih mudah,kalo kamu bikin hidup lebih indah. ${mention}`,
+      `Anjing menggonggong khafilah berlalu. tiap hari bengong mikirin kamu melulu. ${mention}`
     ]
 
     gifList = [
@@ -15,7 +20,8 @@ module.exports = {
       'https://cdn.nekos.life/kiss/kiss_014.gif',
       'https://cdn.nekos.life/kiss/kiss_113.gif',
       'https://cdn.nekos.life/kiss/kiss_094.gif',
-      'https://cdn.nekos.life/kiss/kiss_094.gif'
+      'https://cdn.nekos.life/kiss/kiss_094.gif',
+      'https://cdn.nekos.life/kiss/kiss_075.gif'
     ]
 
 
