fe03df9f8505f24947f2ddd27b962bce002e0c6b
gori <hafidzdev@gmail.com>
2021-08-01 15:48:26 +0700

    adding dontenv depedency

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const RunningServer = require("./server");
 const client = new Discord.Client();
 const fs = require('fs');
+require('dotenv').config();
 
 client.commands = new Discord.Collection();
 
